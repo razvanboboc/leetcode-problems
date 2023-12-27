be2338bd9f6e35c0313a7b705004070832fa25d2
razvanboboc <rzvn11@gmail.com>
2023-12-28 01:01:52 +0200

    Commit first problem

---
+++ Arrays & Hashing/contains-duplicate.py
@@ -0,0 +1,34 @@
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+## Sol 1 - Hash Map
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = {} 
+        for num in nums: 
+            if num in seen and seen[num] >= 1:
+                return True 
+            seen[num] = seen.get(num, 0) + 1
+        return False
+
+## Sol 2 - Hash Set
+
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         seen = set() 
+#         for num in nums: 
+#             if num in seen:
+#                 return True
+#             seen.add(num)
+#         return False
+
+## Sol 3 - Sort Array
+
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         nums.sort()
+#         n = len(nums)
+#         for i in range(n - 1):
+#             if nums[i] == nums[i + 1]:
+#                 return True
+#         return False
