a5122e76755822ae6d53368186607bd885252b55
razvanboboc <rzvn11@gmail.com>
2024-01-08 00:18:48 +0200

    Added better runtime solution for encode and decode strings problem

--- Arrays & Hashing/encode-and-decode-strings.py
+++ Arrays & Hashing/encode-and-decode-strings.py
@@ -1,26 +1,48 @@
 # https://leetcode.com/problems/encode-and-decode-strings/description/
 
+# Sol 1 - O(n)
+
 class Codec:
     def encode(self, strs: List[str]) -> str:
-        encoded = '' 
+        res = ''
         for string in strs:
-            encoded += string.replace('/', '//') + '/:'
-        return encoded
+            res += str(len(string)) + '#' + string
+        return res
 
     def decode(self, s: str) -> List[str]:
-        decodedArray = []
-        decodedWord = ''
-        i = 0
+        decoded, i = [], 0
         while i < len(s):
-            if s[i: i + 2] == '//':
-                decodedWord += s[i]
-                i += 2
-            elif s[i: i + 2] == '/:':
-                i += 2
-                decodedArray.append(decodedWord)
-                decodedWord = ''
-            else: 
-                decodedWord += s[i]
-                i += 1
+            j = i
+            while s[j] != '#':
+                j += 1
+            length = int(s[i : j])
+            decoded.append(s[j + 1: j + 1 + length])
+            i = j + 1 + length
+        return decoded
 
-        return decodedArray
+# Sol 2 - O(n), worse runtime
+
+# class Codec:
+#     def encode(self, strs: List[str]) -> str:
+#         encoded = '' 
+#         for string in strs:
+#             encoded += string.replace('/', '//') + '/:'
+#         return encoded
+# 
+#     def decode(self, s: str) -> List[str]:
+#         decodedArray = []
+#         decodedWord = ''
+#         i = 0
+#         while i < len(s):
+#             if s[i: i + 2] == '//':
+#                 decodedWord += s[i]
+#                 i += 2
+#             elif s[i: i + 2] == '/:':
+#                 i += 2
+#                 decodedArray.append(decodedWord)
+#                 decodedWord = ''
+#             else: 
+#                 decodedWord += s[i]
+#                 i += 1
+# 
+#         return decodedArray
